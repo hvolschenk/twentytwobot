fd9be71e76ff826dcbeb7909e01e8b3d85516ff2
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-08-08 23:56:48 +0200

    feat: add raided command when channel gets raided

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const tmi = require('tmi.js');
 
 const join = require('./commands/join');
+const raided = require('./commands/raided');
 const shoutout = require('./commands/shoutout');
 const commandLogCreate = require('./database/commandLogCreate');
 const getDatabaseConnection = require('./shared/getDatabaseConnection');
@@ -13,6 +14,9 @@ twitchClient.connect();
 
 // When a user joins the channel
 twitchClient.on('join', join);
+
+// When the channel gets raided (this might not work)
+twitchClient.on('raided', raided);
 
 // Any standard message
 twitchClient.on('chat', async (channel, tags, message, self) => {
