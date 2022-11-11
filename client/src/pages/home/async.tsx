befac0fba7302664a51d9a4aa376206d9fe98b56
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-11-11 09:49:07 +0100

    chore(client): implement proper code splitting per route

--- client/src/pages/home/async.tsx
+++ client/src/pages/home/async.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import Home from './index';
+const HomeLazy = React.lazy(() => import('./index'));
 
 const HomeAsync: React.FC = () => (
   <React.Suspense fallback={<p>Loading home page</p>}>
-    <Home />
+    <HomeLazy />
   </React.Suspense>
 );
 
