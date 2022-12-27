00e2a018860a30a9cb2670634a132811164f9c2f
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-12-27 23:19:16 +0100

    chore(client): add optional actions to page-title component

--- client/src/components/PageTitle.tsx
+++ client/src/components/PageTitle.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import React from 'react';
@@ -11,32 +12,48 @@ interface PageTitleBreadcrumb {
 }
 
 interface PageTitleProps {
+  actions?: React.ReactNode;
   breadcrumbs?: PageTitleBreadcrumb[];
   title: string;
 }
 
-const PageTitle: React.FC<PageTitleProps> = ({ breadcrumbs, title }) => (
+const PageTitle: React.FC<PageTitleProps> = ({
+  actions,
+  breadcrumbs,
+  title,
+}) => (
   <Box marginBottom={2}>
-    {breadcrumbs && breadcrumbs.length > 0 && (
-      <Breadcrumbs>
-        {breadcrumbs.map((breadcrumb) => {
-          if (breadcrumb.url) {
-            return (
-              <Link
-                component={RouterLink}
-                key={breadcrumb.url}
-                to={breadcrumb.url}
-              >
-                {breadcrumb.title}
-              </Link>
-            );
-          }
-          return (
-            <Typography key={breadcrumb.title}>{breadcrumb.title}</Typography>
-          );
-        })}
-      </Breadcrumbs>
-    )}
+    <Grid container spacing={2}>
+      <Grid item xs>
+        {breadcrumbs && breadcrumbs.length > 0 && (
+          <Breadcrumbs>
+            {breadcrumbs.map((breadcrumb) => {
+              if (breadcrumb.url) {
+                return (
+                  <Link
+                    component={RouterLink}
+                    key={breadcrumb.url}
+                    to={breadcrumb.url}
+                  >
+                    {breadcrumb.title}
+                  </Link>
+                );
+              }
+              return (
+                <Typography key={breadcrumb.title}>
+                  {breadcrumb.title}
+                </Typography>
+              );
+            })}
+          </Breadcrumbs>
+        )}
+      </Grid>
+      {actions && (
+        <Grid item xs="auto">
+          {actions}
+        </Grid>
+      )}
+    </Grid>
     <Typography variant="h4">{title}</Typography>
   </Box>
 );
